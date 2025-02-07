ef64ca72ab4a8cb47a8d114422c62fd86c78d57c
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-02-07 14:56:46 -0300

    Se agrega el paquete laravel-auditing para implementar las auditorias

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use OwenIt\Auditing\Contracts\Auditable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements Auditable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, SoftDeletes;
+    use \OwenIt\Auditing\Auditable;
 
     /**
      * La primary key asociada a la tabla.
