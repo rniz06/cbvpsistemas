84aeef1eb938bad8c4c4c6a7120e33a36c7fbad6
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-02-07 17:51:19 -0300

    Se agrega el campo codigo a la tabla users para el login y se actualiza el archivo README

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable implements Auditable
      */
     protected $fillable = [
         'personal_id',
+        'codigo',
         'name',
         'email',
         'password',
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id('id_user');
             $table->integer('personal_id')->nullable();
+            $table->string('codigo', 10)->nullable();
             $table->string('name')->nullable();
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
