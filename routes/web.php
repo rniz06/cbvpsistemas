4a3747b00e30981d0da85d0565910bed1b29268c
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-02-08 08:25:48 -0300

    Se agrega el paquete laravel-adminlte con el andamiaje de autenticación de laravel y se configura el login con el campo codigo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+//use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Auth::routes();
+Auth::routes([
+    'register' => false, // Desactivar route Register...
+    'reset' => false, // Desactivar route Reset Password...
+    'verify' => false, // Desactivar route Email Verification...
+]);
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
