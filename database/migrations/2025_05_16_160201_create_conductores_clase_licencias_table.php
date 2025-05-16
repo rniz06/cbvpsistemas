b2b896c132636c35ef177dd3239c93c619f9674e
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-05-16 17:01:51 -0300

    Agregar migraciones para el modulo de gadma conductores

---
+++ database/migrations/2025_05_16_160201_create_conductores_clase_licencias_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conductores_clase_licencias', function (Blueprint $table) {
+            $table->id('idconductor_clase_licencia');
+            $table->string('clase', 45);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conductores_clase_licencias');
+    }
+};
