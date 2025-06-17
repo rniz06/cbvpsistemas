89984c0b31cbc6cf67037db3876201d146176d4b
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-06-17 19:06:03 -0300

    Agrega migraciones tablas-prefijo GRAL: companias,regiones,departamentos,ciudades

---
+++ database/migrations/2025_06_17_183050_create_GRAL_regiones_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('GRAL_regiones', function (Blueprint $table) {
+            $table->id('id_region');
+            $table->integer('region');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('GRAL_regiones');
+    }
+};
