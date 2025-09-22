2fbec3f3af61f68215f4004342a8dae16e1ced35
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-09-22 08:55:10 -0300

    feat: Agrega badge de colores al mostrar el estado del conductor

--- resources/views/livewire/materiales/conductores/index.blade.php
+++ resources/views/livewire/materiales/conductores/index.blade.php
@@ -10,24 +10,24 @@
         </x-slot>
 
         <x-slot name="cabeceras">
-            <th><x-adminlte-input name="" wire:model.live.debounce.150ms="buscarCodigo" label="Codigo:" /></th>
-            <th><x-adminlte-input name="" wire:model.live.debounce.150ms="buscarNombrecompleto"
+            <th><x-adminlte-input name="" wire:model.live.debounce.200ms="buscarCodigo" label="Codigo:" /></th>
+            <th><x-adminlte-input name="" wire:model.live.debounce.200ms="buscarNombrecompleto"
                     label="Nombre Completo:" /></th>
-            <th><x-adminlte-select name="" wire:model.live.debounce.150ms="buscarCompaniaId" label="Compañia:">
+            <th><x-adminlte-select name="" wire:model.live.debounce.200ms="buscarCompaniaId" label="Compañia:">
                     <option value="">-- TODOS --</option>
                     @foreach ($companias as $compania)
                         <option value="{{ $compania->id_compania }}">{{ $compania->compania ?? 'S/D' }}</option>
                     @endforeach
                 </x-adminlte-select>
             </th>
-            <th><x-adminlte-select name="" wire:model.live.debounce.150ms="buscarEstadoId" label="Estado:">
+            <th><x-adminlte-select name="" wire:model.live.debounce.200ms="buscarEstadoId" label="Estado:">
                     <option value="">-- TODOS --</option>
                     @foreach ($estados as $estado)
                         <option value="{{ $estado->id_conductor_estado }}">{{ $estado->estado ?? 'S/D' }}</option>
                     @endforeach
                 </x-adminlte-select>
             </th>
-            <th><x-adminlte-select name="" wire:model.live.debounce.150ms="buscarClaseLicenciaId"
+            <th><x-adminlte-select name="" wire:model.live.debounce.200ms="buscarClaseLicenciaId"
                     label="Clase Licencia:">
                     <option value="">-- TODOS --</option>
                     @foreach ($licencias as $licencia)
@@ -44,7 +44,12 @@
                 <td>{{ $conductor->codigo ?? 'S/D' }}</td>
                 <td>{{ $conductor->nombrecompleto ?? 'S/D' }}</td>
                 <td>{{ $conductor->compania ?? 'S/D' }}</td>
-                <td>{{ $conductor->estado ?? 'S/D' }}</td>
+                <td>
+                    <span class="badge bg-{{ $conductor->estado === 'ACTIVO' ? 'success' : 'danger' }}">
+                        {{ $conductor->estado ?? 'S/D' }}
+                    </span>
+                </td>
+
                 <td>{{ $conductor->clase_licencia ?? 'S/D' }}</td>
                 <td>
                     @can('Conductores Ver')
