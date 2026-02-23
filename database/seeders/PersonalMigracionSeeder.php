7214b45616c5f07360221b4ac16c226cf5343dee
rniz06 <ronaldalexisniznunez@gmail.com>
2026-02-23 19:38:00 -0300

    Seeder para migracion actualizacion/insercion masiva de excel del dpto de personal

--- database/seeders/PersonalMigracionSeeder.php
+++ database/seeders/PersonalMigracionSeeder.php
@@ -26,6 +26,25 @@ class PersonalMigracionSeeder extends Seeder
 
             $falta_actualizar = DB::table('personal')
                 ->where('estado_actualizar_id', 1)
+                ->whereNotIn('codigo', [
+                    # CODIGOS DUPLICADOS
+                    17,
+                    88,
+                    438,
+                    597,
+                    2823,
+                    3129,
+                    3131,
+                    3132,
+                    3133,
+                    3134,
+                    3137,
+                    3432,
+                    3442,
+                    6453,
+                    9821,
+                    10602
+                ])
                 ->select('idpersonal', 'codigo', 'fecha_juramento')
                 ->get();
 
@@ -55,7 +74,7 @@ class PersonalMigracionSeeder extends Seeder
                         'nacionalidad_id'       => $migrado->nacionalidad_id,
                         'grupo_sanguineo_id'    => $migrado->grupo_sanguineo_id,
                         'tipo_documento_id'     => $migrado->tipo_documento_id,
-                        'estado_actualizar_id'  => 2,
+                        'estado_actualizar_id'  => 3, # Actualizado Excel
                         'ultima_actualizacion'  => now(),
                     ]);
             }
@@ -73,6 +92,16 @@ class PersonalMigracionSeeder extends Seeder
                 $existe = DB::table('personal')
                     ->where('codigo', $migrado->codigo)
                     ->where('fecha_juramento', $migrado->fecha_juramento)
+                    // ->whereNotIn('codigo', [
+                    //     5, 6, 14, 20, 21, 24, 77, 100, 107, 110,
+                    //     128, 136, 177, 191, 198, 218, 227, 234,
+                    //     275, 283, 285, 287, 295, 296, 309, 316,
+                    //     318, 323, 326, 343, 373, 376, 383, 389,
+                    //     397, 398, 402, 429, 433, 444, 542, 567,
+                    //     587, 597, 608, 746, 2823, 2907, 3123,
+                    //     3124, 3127, 3129, 3131, 3132, 3133,
+                    //     3134, 5828, 6614
+                    // ])
                     ->exists();
 
                 if ($existe) {
@@ -93,7 +122,7 @@ class PersonalMigracionSeeder extends Seeder
                     'nacionalidad_id'       => $migrado->nacionalidad_id,
                     'grupo_sanguineo_id'    => $migrado->grupo_sanguineo_id,
                     'tipo_documento_id'     => $migrado->tipo_documento_id,
-                    'estado_actualizar_id'  => 2,
+                    'estado_actualizar_id'  => 4, # Insert Excel
                     'ultima_actualizacion'  => now(),
                 ]);
             }
