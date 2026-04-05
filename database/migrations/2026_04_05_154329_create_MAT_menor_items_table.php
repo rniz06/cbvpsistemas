908714f8b3ab8b5042f104b36b46bbc71b9759ab
rniz06 <ronaldalexisniznunez@gmail.com>
2026-04-05 16:53:21 -0300

    feat(materiales.menor) agrega migraciones

---
+++ database/migrations/2026_04_05_154329_create_MAT_menor_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('MAT_menor_items', function (Blueprint $table) {
+            $table->id('id_menor_item');
+            $table->foreignId('componente_id')->constrained('MAT_menor_componentes', 'id_menor_componente')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('estado_id')->constrained('MAT_operatividad', 'id_operatividad')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('compania_id')->constrained('GRAL_companias', 'id_compania')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('creadoPor')->nullable()->constrained('users', 'id_usuario')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('actualizadoPor')->nullable()->constrained('users', 'id_usuario')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('MAT_menor_items');
+    }
+};
